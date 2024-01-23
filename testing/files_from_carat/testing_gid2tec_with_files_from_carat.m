06ac912e75433e8e3464fc58c3109b44c732d3c3
gdenayer <denayer@hsu-hh.de>
2024-01-23 17:22:00 +0100

    Made some minor changes in the headers of the tecplot file. Add some files for testing purposes.

---
+++ testing/files_from_carat/testing_gid2tec_with_files_from_carat.m
@@ -0,0 +1,8 @@
+clc
+clear all
+
+basename='test_air_inflated_structure_FEM_triangle_1926_steady'
+gid2tec(basename)
+
+basename='Tstruct_dynamic_quad_fine'
+gid2tec(basename)
---
+++ testing/files_from_empire/testing_gid2tec_with_files_from_empire.m
@@ -0,0 +1,8 @@
+clc
+clear all
+
+basename='hypar_testMapper_mortar1_meshA'
+gid2tec(basename)
+
+basename='test_air_inflated_structure_100000_FSI_medium_grid_FEM_1926'
+gid2tec(basename)
